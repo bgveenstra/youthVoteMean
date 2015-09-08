a566cf386606f7a0d3cf93205c73513be48a58e5
rchau05 <rnchau@mail.sfsu.edu>
2015-09-07 23:56:48 -0700

    changes in signup.  put in correct input fields

--- models/user.js
+++ models/user.js
@@ -26,5 +26,41 @@ UserSchema.pre('save', function (next) {
   next();
 });
 
+UserSchema.statics.createSecure = function (email, password, callback) {
+ // `this` references our schema
+ // store it in variable `that` because `this` changes context in nested callbacks
+ var that = this;
+
+ // hash password user enters at sign up
+ bcrypt.genSalt(function (err, salt) {
+   bcrypt.hash(password, salt, function (err, hash) {
+     console.log(hash);
+
+     // create the new user (save to db) with hashed password
+     that.create({
+       email: email,
+       passwordDigest: hash
+     }, callback);
+   });
+ });
+};
+
+UserSchema.statics.authenticate = function (email, password, callback) {
+ this.findOne({email: email}, function (err, user) {
+   console.log(user);
+   if (user === null) {
+     callback('Can\'t find user with email ' + email, user);
+   } else if (user.checkPassword(password)) {
+     callback(null, user);
+   }
+ });
+};
+
+UserSchema.methods.checkPassword = function (password) {
+ return password == this.password;
+};
+
+
+
 // EXPORT POST MODEL
 mongoose.model('User', UserSchema);
