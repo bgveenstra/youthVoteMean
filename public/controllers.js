000bb57e58819028b179a867f7b7e49c93921676
rchau05 <rnchau@mail.sfsu.edu>
2015-09-09 14:27:48 -0700

    added google chart scripts

--- public/controllers.js
+++ public/controllers.js
@@ -75,36 +75,124 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
         }
   })
 
-  //POSTS
-  .controller('TodosIndexCtrl', function ($scope, $location, Post, Auth) {
-    Post.query(
-      function(data) {
-        $scope.todos = data
+  .controller('ResultsCtrl', ['$scope', function ($scope) {
+    $scope.chartObject = {
+      "type": "AreaChart",
+      "displayed": false,
+      "data": {
+        "cols": [
+          {
+            "id": "month",
+            "label": "Month",
+            "type": "string",
+            "p": {}
+          },
+          {
+            "id": "laptop-id",
+            "label": "Laptop",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "desktop-id",
+            "label": "Desktop",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "server-id",
+            "label": "Server",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "cost-id",
+            "label": "Shipping",
+            "type": "number"
+          }
+        ],
+        "rows": [
+          {
+            "c": [
+              {
+                "v": "January"
+              },
+              {
+                "v": 19,
+                "f": "42 items"
+              },
+              {
+                "v": 12,
+                "f": "Ony 12 items"
+              },
+              {
+                "v": 7,
+                "f": "7 servers"
+              },
+              {
+                "v": 4
+              }
+            ]
+          },
+          {
+            "c": [
+              {
+                "v": "February"
+              },
+              {
+                "v": 13
+              },
+              {
+                "v": 1,
+                "f": "1 unit (Out of stock this month)"
+              },
+              {
+                "v": 12
+              },
+              {
+                "v": 2
+              }
+            ]
+          },
+          {
+            "c": [
+              {
+                "v": "March"
+              },
+              {
+                "v": 24
+              },
+              {
+                "v": 5
+              },
+              {
+                "v": 11
+              },
+              {
+                "v": 6
+              }
+            ]
+          }
+        ]
       },
-      function(data) {
-        $location.path('/login');
-      }
-    );
-
-    $scope.todo = {};
-    $scope.createTodo = function() {
-      Post.save($scope.todo, 
-        function(data){
-          $scope.todos.push(data);
+      "options": {
+        "title": "Sales per month",
+        "isStacked": "true",
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+          "title": "Sales unit",
+          "gridlines": {
+            "count": 10
+          }
         },
-        function(data) {
-          alert("there was a problem saving your todo");
+        "hAxis": {
+          "title": "Date"
         }
-      );
-      $scope.todo = '';
+      },
+      "formatters": {}
     }
-
-    $scope.deleteTodo = function(todo) {
-      Post.delete({ id: todo._id });
-      var index = $scope.todos.indexOf(todo)
-      $scope.todos.splice(index, 1);
-    }
-  })
+  }])
 
   // CAROUSEL
   .controller('CarouselDemoCtrl', ['$scope', function ($scope) {
